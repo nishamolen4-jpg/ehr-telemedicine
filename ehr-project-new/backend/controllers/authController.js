3aba9a7db0294cf75ab426ec19d9e6157e4f2e88
pc <pc@DESKTOP-9Q3S85E>
2026-05-01 23:55:02 +0530

    file updated

---
+++ ehr-project-new/backend/controllers/authController.js
@@ -0,0 +1,30 @@
+const Patient = require("../models/Patient");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await Patient.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ msg: "User not found" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Invalid password" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.json({ token });
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
